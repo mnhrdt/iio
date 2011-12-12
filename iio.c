39d0390b6561d0b52eaa511a0d8211e459af75d2
Enric Meinhardt-Llopis <coco@fuchsia.hw.ipol.im>
2011-12-12 14:47:15 +0100

    make gabriele happy

--- iio.c
+++ iio.c
@@ -672,6 +672,8 @@ static void *iio_image_get_data(struct iio_image *x) { return x->data; }
 // data conversion {{{1
 
 #define T0(x) ((x)>0?(x):0)
+#define T8(x) ((x)>0?((x)<0xff?(x):0xff):0)
+#define T6(x) ((x)>0?((x)<0xffff?(x):0xffff):0)
 
 #define CC(a,b) (77*(a)+(b)) // hash number of a conversion pair
 #define I8 IIO_TYPE_INT8
@@ -739,11 +741,11 @@ static void convert_datum(void *dest, void *src, int dest_fmt, int src_fmt)
 	case CC(I8,F8): *(  int8_t*)dest = *(double*)src; break;//iw810
 	case CC(I6,F8): *( int16_t*)dest = *(double*)src; break;//iw810
 	case CC(I2,F8): *( int32_t*)dest = *(double*)src; break;//iw810
-	case CC(U8,F4): *( uint8_t*)dest = T0(*( float*)src); break;//iw810
-	case CC(U6,F4): *(uint16_t*)dest = *( float*)src; break;//iw810
+	case CC(U8,F4): *( uint8_t*)dest = T8(*( float*)src); break;//iw810
+	case CC(U6,F4): *(uint16_t*)dest = T6(*( float*)src); break;//iw810
 	case CC(U2,F4): *(uint32_t*)dest = *( float*)src; break;
-	case CC(U8,F8): *( uint8_t*)dest = *(double*)src; break;//iw810
-	case CC(U6,F8): *(uint16_t*)dest = *(double*)src; break;//iw810
+	case CC(U8,F8): *( uint8_t*)dest = T8(*(double*)src); break;//iw810
+	case CC(U6,F8): *(uint16_t*)dest = T6(*(double*)src); break;//iw810
 	case CC(U2,F8): *(uint32_t*)dest = *(double*)src; break;//iw810
 
 	// to float (typically lossless, except for large integers)
@@ -813,8 +815,8 @@ static void convert_datum(void *dest, void *src, int dest_fmt, int src_fmt)
 	case CC(I8,F6): *(  int8_t*)dest = *(longdouble*)src; break;
 	case CC(I6,F6): *( int16_t*)dest = *(longdouble*)src; break;
 	case CC(I2,F6): *( int32_t*)dest = *(longdouble*)src; break;
-	case CC(U8,F6): *( uint8_t*)dest = *(longdouble*)src; break;
-	case CC(U6,F6): *(uint16_t*)dest = *(longdouble*)src; break;
+	case CC(U8,F6): *( uint8_t*)dest = T8(*(longdouble*)src); break;
+	case CC(U6,F6): *(uint16_t*)dest = T6(*(longdouble*)src); break;
 	case CC(U2,F6): *(uint32_t*)dest = *(longdouble*)src; break;
 	case CC(F4,F6): *(   float*)dest = *(longdouble*)src; break;
 	case CC(F8,F6): *(  double*)dest = *(longdouble*)src; break;
