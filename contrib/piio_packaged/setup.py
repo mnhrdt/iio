a8325fb54b8fe001d5217ad3d3e66db91af13973
Gabriele Facciolo <gfacciol@gmail.com>
2016-07-19 15:30:48 +0200

    detect libraw (only on unix systems) while building piio

--- contrib/piio_packaged/setup.py
+++ contrib/piio_packaged/setup.py
@@ -1,12 +1,25 @@
 from distutils.core import setup, Extension
 
+import os.path
 
-iiomodule = Extension('piio.libiio',  
-    libraries = ['png','jpeg','tiff'],
-    language=['c99'],
-    extra_compile_args = ['-std=c99','-DNDEBUG','-O3'], 
-    sources = ['piio/iio.c','piio/freemem.c']
-   )
+def getmodulesetup():
+   if os.path.exists('/usr/local/include/libraw') or os.path.exists('/usr/include/libraw'):
+      print('LIBRAW detected')
+      iiomodule = Extension('piio.libiio',  
+          libraries = ['png','jpeg','tiff','raw'],
+          language=['c99'],
+   #       extra_compile_args = ['-std=c99','-DNDEBUG','-O3'], 
+          sources = ['piio/iio.c','piio/freemem.c', 'piio/libraw_interface.cpp']
+         )
+   else: 
+      iiomodule = Extension('piio.libiio',  
+          libraries = ['png','jpeg','tiff'],
+          language=['c99'],
+   #       extra_compile_args = ['-std=c99','-DNDEBUG','-O3'], 
+          sources = ['piio/iio.c','piio/freemem.c']
+         )
+   return [iiomodule]
+
 
 
 setup(
@@ -19,5 +32,5 @@ setup(
     url='http://github.com//',
     packages=['piio'],
 #    ext_package='piio',
-    ext_modules = [iiomodule]
+    ext_modules = getmodulesetup()
    )
