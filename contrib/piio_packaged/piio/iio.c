3bc575e3891bcfa5de5eea78720e1b63e7a059fd
Gabriele Facciolo <gfacciol@gmail.com>
2016-08-26 17:48:08 +0200

    missing file

--- contrib/piio_packaged/piio/iio.c
+++ contrib/piio_packaged/piio/iio.c
@@ -3037,6 +3037,8 @@ static int guess_format(FILE *f, char *buf, int *nbuf, int bufmax)
 			return IIO_FORMAT_JPEG;
 		if (b[3]==0xe1 && b[6]=='E' && b[7]=='x')
 			return IIO_FORMAT_JPEG;
+		if (b[3]==0xee || b[3]==0xed) // Adobe JPEG
+			return IIO_FORMAT_JPEG;
 	}
 #endif//I_CAN_HAS_LIBPNG
 
