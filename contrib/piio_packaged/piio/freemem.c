2a916c03b17c448eecb270b4786accc71dcb5823
Gabriele Facciolo <gfacciol@gmail.com>
2016-10-14 10:06:55 +0200

    uniformize freemem.c

--- contrib/piio_packaged/piio/freemem.c
+++ contrib/piio_packaged/piio/freemem.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
 void freemem(void *ptr){
    free(ptr);
@@ -33,5 +34,15 @@ void copy_tile(float *src, int nc, int nr, int nch, float *dst, int x0, int y0, 
       dst[ dst_nch*(i + j*w)+c ] = v;
    }
    }
+   }
+}
+
+#define swap_uint8(x,y) {uint8_t t = x; x = y; y = t;}
+// OpenGL screen buffers are bottom-to-top, files are top-to-bottom
+void reverse_vertically_uint8_buffer_inplace(uint8_t *buff, int w, int h, int nch) {
+   for (int j=0;j<h/2;j++)
+   for (int i=0;i<w;i++)
+   for (int c=0;c<nch;c++) {
+      swap_uint8(buff[nch*(i + (h-j-1)*w)+c] ,  buff[nch*(i + j*w)+c]);
    }
 }
