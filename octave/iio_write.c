a4bc4096b3bd213b3c18370ef7281ec4ba1be9ff
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2017-12-08 16:31:15 +0100

    octave interface: remove debugging stuff

--- octave/iio_write.c
+++ octave/iio_write.c
@@ -1,8 +1,5 @@
 // octave/iio interface
 
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <mex.h>
 #include "iio.h"
 
@@ -15,16 +12,10 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	const mwSize *dims = mxGetDimensions(prhs[1]);
 	int ndims = mxGetNumberOfDimensions(prhs[1]);
 	if (ndims >= 3) ndims = 3;
-	fprintf(stderr, "ndims = %d\n", ndims);
 
 	int s[3] = {1, 1, 1};
 	for (int i = 0; i < ndims; i++)
 		s[i] = dims[i];
-
-
-	for (int i = 0; i < ndims; i++)
-		fprintf(stderr, "s[%d] = %d\n", i, s[i]);
-
 
 	double *x = mxGetPr(prhs[1]);
 	char *f = mxArrayToString(prhs[0]);
