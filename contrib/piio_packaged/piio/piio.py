cc98664e19cd6c5d4452a9784f7b8fa90dd7f54e
Gabriele Facciolo <gfacciol@gmail.com>
2018-07-02 09:42:55 +0200

    fix fortrans-style array export

--- contrib/piio_packaged/piio/piio.py
+++ contrib/piio_packaged/piio/piio.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import os,sys,ctypes
 
 #if sys.platform.startswith('win'):
@@ -29,7 +30,10 @@ def read(filename):
    nch=c_int()
    
    iioread.restype = c_void_p  # it's like this
-   tptr = iioread(bytes(filename,encoding="utf8"),byref(w),byref(h),byref(nch))
+   if sys.version_info[0] < 3:
+      tptr = iioread(bytes(filename).encode("utf-8"),byref(w),byref(h),byref(nch))
+   else:
+      tptr = iioread(bytes(filename,encoding="utf8"),byref(w),byref(h),byref(nch))
    c_float_p = POINTER(c_float)       # define a new type of pointer
    ptr = cast(tptr, c_float_p)
    #print w,h,nch
@@ -55,6 +59,7 @@ def write(filename,data):
    '''
    from ctypes import c_char_p, c_int, c_float
    from numpy.ctypeslib import ndpointer
+   from numpy import ascontiguousarray
 
    iiosave = libiio.iio_save_image_float_vec
 
@@ -66,7 +71,7 @@ def write(filename,data):
 
    iiosave.restype = None
    iiosave.argtypes = [c_char_p, ndpointer(c_float),c_int,c_int,c_int]
-   iiosave(str(filename).encode("utf8"), data.astype('float32'), w, h, nch)
+   iiosave(str(filename).encode("utf8"), ascontiguousarray(data, dtype='float32'), w, h, nch)
 
 
 #d = piio.read('testimg.tif')
