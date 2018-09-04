ad4226d4511a7de0f377b38ab398ac6730bc2d92
Charles Hessel <hesselcharles@gmail.com>
2018-09-04 17:17:48 +0200

    Octave: write images that are not of type double
    Saving with iio_write an image which type was not double caused either a
    segfault or a nasty result; this is now fixed.
    The input can henceforth be double, single, uint16, int16, uint8, int8
    or int32. For other data types the function will fail elegantly.

--- contrib/octave/example_iio.m
+++ contrib/octave/example_iio.m
@@ -1,11 +1,19 @@
-# read an image using iio
-x = iio_read("lena.png");
+% read an image using iio
+x = iio_read('lena.png');
 
-# compute negative image
+% compute negative image
 y = 255 - x;
 
-# save an array into a png file
-iio_write("neg_lena.png", y);
+% save an array into a png file
+iio_write('neg_lena.png', y);
+iio_write('neg_lena_double.tiff',       y );
+iio_write('neg_lena_single.tiff',single(y));
+iio_write('neg_lena_uint16.tiff',uint16(y));
+iio_write('neg_lena_int8.tiff'  ,int8(  y/2));
+iio_write('neg_lena_int16.tiff' ,int16( y));
+iio_write('neg_lena_int32.tiff' ,int32( y));
+% iio_write('neg_lena_int64.tiff' ,uint32(y));
+iio_write('neg_lena_uint8.tiff' ,uint8( y));
 
-# save the original image using imwrite
-imwrite(uint8(x), "lena_imwrite.png");
+% save the original image using imwrite
+imwrite(uint8(x), 'lena_imwrite.png');
--- contrib/octave/iio_write.c
+++ contrib/octave/iio_write.c
@@ -18,14 +18,120 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	int w = ndims > 1 ? dims[1] : 1;
 	int d = ndims > 2 ? dims[2] : 1;
 
-	double *X = mxGetPr(prhs[1]);
-	double *x = malloc(w*h*d*sizeof*x);
-	for (int j = 0; j < h; j++)
-	for (int i = 0; i < w; i++)
-	for (int k = 0; k < d; k++)
-		x[(j*w+i)*d + k] = X[w*h*k + (i*h+j)];
 	char *f = mxArrayToString(prhs[0]);
-	iio_write_image_double_vec(f, x, w, h, d);
+
+	switch(mxGetClassID(prhs[1]))
+	{
+		case mxDOUBLE_CLASS:
+		{
+			double *X = mxGetPr(prhs[1]);
+			double *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = X[w*h*k + (i*h+j)];
+			iio_write_image_double_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxSINGLE_CLASS:
+		{
+			float *X = (float*) mxGetPr(prhs[1]);
+			float *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = X[w*h*k + (i*h+j)];
+			iio_write_image_float_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxINT8_CLASS:
+		{
+			int8_t *X = (int8_t*) mxGetPr(prhs[1]);
+			int *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = (int)X[w*h*k + (i*h+j)];
+			iio_write_image_int_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxINT16_CLASS:
+		{
+			int16_t *X = (int16_t*) mxGetPr(prhs[1]);
+			int *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = (int)X[w*h*k + (i*h+j)];
+			iio_write_image_int_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxINT32_CLASS:
+		{
+			int32_t *X = (int32_t*) mxGetPr(prhs[1]);
+			int *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = (int)X[w*h*k + (i*h+j)];
+			iio_write_image_int_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxUINT8_CLASS:
+		{
+			uint8_t *X = (uint8_t*) mxGetPr(prhs[1]);
+			uint8_t *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = X[w*h*k + (i*h+j)];
+			iio_write_image_uint8_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxUINT16_CLASS:
+		{
+			uint16_t *X = (uint16_t*) mxGetPr(prhs[1]);
+			uint16_t *x = malloc(w*h*d*sizeof*x);
+			for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+			for (int k = 0; k < d; k++)
+				x[(j*w+i)*d + k] = X[w*h*k + (i*h+j)];
+			iio_write_image_uint16_vec(f, x, w, h, d);
+			free(x);
+			break;
+		}
+
+		case mxLOGICAL_CLASS:
+		case mxUINT32_CLASS:
+		case mxINT64_CLASS:
+		case mxUINT64_CLASS:
+			mexErrMsgTxt("Classes Logical, uint32, int64 and uint64 not "
+					"handled. Try with a more common one.");
+			break;
+
+		case mxUNKNOWN_CLASS:
+		case mxCELL_CLASS:
+		case mxSTRUCT_CLASS:
+		case mxCHAR_CLASS:
+		case mxVOID_CLASS:
+		case mxFUNCTION_CLASS:
+			mexErrMsgTxt("Invalid class.");
+			break;
+
+		default:
+			mexErrMsgTxt("Class not recognized.");
+	}
 	mxFree(f);
-	free(x);
 }
