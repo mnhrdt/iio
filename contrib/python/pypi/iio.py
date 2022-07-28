0f2900ad8231ac5c401811e92ec40374ddffae4e
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2022-07-28 12:08:42 +0200

    pypi cleanup exported symbols

--- contrib/python/pypi/iio.py
+++ contrib/python/pypi/iio.py
@@ -5,45 +5,41 @@
 
 
 import os.path
-import ctypes
-from ctypes import *
+from ctypes import CDLL, POINTER, c_float, c_int, c_char_p, c_void_p
 from ctypes.util import find_library
-import numpy as np
+import numpy
 
 libiio = CDLL(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'libiio.so'))
 
-libc = ctypes.CDLL(find_library('c'))
-libc.free.argtypes = [ctypes.c_void_p]
-libc.free.restype = ctypes.c_void_p
+libc = CDLL(find_library('c'))
+libc.free.argtypes = [c_void_p]
+libc.free.restype = c_void_p
 
 iioread = libiio.iio_read_image_float_vec
 iioread.argtypes = [c_char_p, POINTER(c_int), POINTER(c_int), POINTER(c_int)]
 iioread.restype = POINTER(c_float)
 
 iiosave = libiio.iio_write_image_float_vec
-iiosave.argtypes = [c_char_p, np.ctypeslib.ndpointer(c_float),c_int,c_int,c_int]
+iiosave.argtypes = [c_char_p, numpy.ctypeslib.ndpointer(c_float),c_int,c_int,c_int]
 iiosave.restype = None
 
 def read(filename):
-   w = c_int()
-   h = c_int()
-   nch = c_int()
+	w = c_int()
+	h = c_int()
+	nch = c_int()
 
-   ptr = iioread(filename.encode('utf-8'), w, h, nch)
-   data = np.ctypeslib.as_array(ptr, (h.value, w.value, nch.value)).copy()
-   libc.free(ptr)
-   return data
+	ptr = iioread(filename.encode('utf-8'), w, h, nch)
+	data = numpy.ctypeslib.as_array(ptr, (h.value, w.value, nch.value)).copy()
+	libc.free(ptr)
+	return data
 
 def write(filename, data):
-   h = data.shape[0]
-   w = len(data.shape) <= 1 and 1 or data.shape[1]
-   nch = len(data.shape) <= 2 and 1 or data.shape[2]
+	h = data.shape[0]
+	w = len(data.shape) <= 1 and 1 or data.shape[1]
+	nch = len(data.shape) <= 2 and 1 or data.shape[2]
 
-   data = np.ascontiguousarray(data, dtype='float32')
-   iiosave(filename.encode('utf-8'), data, w, h, nch)
+	data = numpy.ascontiguousarray(data, dtype='float32')
+	iiosave(filename.encode('utf-8'), data, w, h, nch)
 
-__all__ = {
-    'read': read,
-    'write': write,
-}
+__all__ = [ "read", "write" ]
 
